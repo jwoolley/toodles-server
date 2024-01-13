8ef70d97da63f27d02489dc32ad2f51df4e2c343
Justin Woolley <jwoolley@users.noreply.github.com>
2024-01-13 01:01:57 +0000

    add pm2 log timestamps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
 			args: "run server",
 			ignore_watch: ["node_modules", ".git"],
 			watch_delay: 1000,
+   		log_date_format: "|YYYY-MM-DD|HH:mm:ss.SSS|",
 			env: {
 				PORT: EXPRESS_SERVER_PORT,
 				DB_PATH: '/home/devuser/databases/toodles-dev.db',
@@ -23,6 +24,7 @@ module.exports = {
 			cron_restart: "0 0 * * *",
 			autorestart: false,
 			watch: false,
+   		log_date_format: "|YYYY-MM-DD|HH:mm:ss.SSS|",
 			env: {
 				DB_PATH: '/home/devuser/databases/toodles-dev.db',
 				DAILY_PUZZLE_TABLE_NAME: 'daily_puzzles'
